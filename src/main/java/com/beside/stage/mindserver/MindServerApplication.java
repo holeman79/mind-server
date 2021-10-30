e740f6fe7cd2b9c862cfce70f5a7577fca77fb4e
조원진 <>
2021-10-30 17:05:25 +0900

    Initial commit

---
+++ src/main/java/com/beside/stage/mindserver/MindServerApplication.java
@@ -0,0 +1,13 @@
+package com.beside.stage.mindserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beside/stage/mindserver/MindServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beside.stage.mindserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
