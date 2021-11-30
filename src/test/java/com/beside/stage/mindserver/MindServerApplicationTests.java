71504f91d31f780813444d8ffc5570df1ab0f736
조원진 <>
2021-11-30 14:58:28 +0900

    test(QuestionReadDocumentTest) : Rest Docs 예제 내용 값 변경

--- src/test/java/com/beside/stage/mindserver/MindServerApplicationTests.java
+++ src/test/java/com/beside/stage/mindserver/MindServerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.beside.stage.mindserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MindServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
